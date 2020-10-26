a76bf53159189cd3e26a908027caf3c22a7a288a
nadiayusof93 <nadiayusof93@gmail.com>
2020-10-27 02:10:42 +0800

    Update main.js
    remove call function

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const uri = "mongodb://localhost:27017"
 
 //saveQueryData();
 //connect to mongoDb
-saveQueryData();
 async function saveQueryData(){
     const client = new MongoClient(uri);
     try{
